a8deb95f5b96e0e1c6de36f639aecb27a7acb06e
EmanuelySoares <soares.emanuely@escola.pr.gov.br>
2025-03-12 12:24:31 -0300

    Add files via upload

---
+++ Atividade 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int eh_bissexto(int ano) {
+    if ((ano % 4 == 0 && ano % 100 != 0) || ( ano % 400 ==0)) {
+    return 1; 
+    {
+    return 0;
+{
+    int main() {
+        int ano;
+        
+    printf("2008: ");
+    scanf("%d",&ano);
+    if (eh_bissexto(ano)) {
+        printf("0 ano %d é bissexto.\n", ano);
+        else {
+            printf(0 ano %d não é bissexto. \n" , ano):
+          return 0;
